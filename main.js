dfaa67161d041ebdf7fbfe1d8731b23ece650c56
shibadog0410 <shibadog0410@gmail.com>
2021-04-27 23:32:12 +0900

    fix: vue-bind

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'First Application!',
-        list: ['apple', 'banana', 'strawberry'],
-        show: true,
-    },
-
-    methods: {
-        handleClick: function (event) {
-            alert(event.target);
-        }
-    },
+        ok: true,
+    }
 });

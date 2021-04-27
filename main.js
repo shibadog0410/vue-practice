26eef6dcf978580a8be73c24e3816be3d651e34e
shibadog0410 <shibadog0410@gmail.com>
2021-04-28 00:42:13 +0900

    fix: list&bootstrap

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var app = new Vue({
     el: '#app',
     data: {
-        ok: true,
+        list: [
+            { id: 1, name: 'スライム', hp: 100 },
+            { id: 2, name: 'ドブリン', hp: 200 },
+            { id: 3, name: 'ドラゴン', hp: 500 },
+        ],
     }
 });

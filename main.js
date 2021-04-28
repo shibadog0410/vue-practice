c12501fe443aef50148969a62ddd3032aeb87f20
shibadog0410 <shibadog0410@gmail.com>
2021-04-28 23:20:06 +0900

    fix: monster list with bootstrap

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
-var app = new Vue({
+new Vue({
     el: '#app',
     data: {
-        list: [
-            { id: 1, name: 'スライム', hp: 100 },
-            { id: 2, name: 'ドブリン', hp: 200 },
-            { id: 3, name: 'ドラゴン', hp: 500 },
-        ],
+        name: 'Chimera',
+        list: []
+    },
+
+    mounted: function () {
+        console.log(this.$refs.input);
+    },
+
+    created: function () {
+        axios.get('list.json').then(function (response) {
+            this.list = response.data;
+        }.bind(this)).catch(function(e) {
+            console.error(e);
+        })
+    },
+
+    methods: {
+        doAdd: function () {
+            var max = this.list.reduce(function (a, b) {
+                return a > b.id ? a : b.id;
+            }, 0);
+
+            this.list.push({
+                id: max + 1,
+                name: this.name,
+                hp: 500,
+            });
+        },
+
+        doRemove: function (index) {
+            this.list.splice(index, 1);
+        },
+
+        doAttack: function (index) {
+            this.list[index].hp -= 10;
+        }
     }
 });

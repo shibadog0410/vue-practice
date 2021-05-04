f39f16000b735221d7416937e1f848e64995d5a6
shibadog0410 <shibadog0410@gmail.com>
2021-05-05 00:42:25 +0900

    fix: splited single file components.

---
+++ src/components/Scroll.vue
@@ -0,0 +1,18 @@
+<template>
+<div v-on:click="scrollTop" class="my-4 btn btn-light btn-outline-dark font-weight-bold">
+    Scroll Top
+</div>
+</template>
+
+<script>
+var scroll = new SmoothScroll();
+
+export default {
+    name: 'Scroll',
+    methods: {
+        scrollTop: function () {
+            scroll.animateScroll(0);
+        },
+    }
+}
+</script>

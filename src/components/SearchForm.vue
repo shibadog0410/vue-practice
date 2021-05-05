d2919b9e4a7586921f086cce3dfd0e890c387046
shibadog0410 <shibadog0410@gmail.com>
2021-05-05 22:31:06 +0900

    fix layout

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,28 +1,24 @@
 <template>
 <div class="row mx-auto py-3 px-0 bg-light rounded">
-    <div class="col-3 input-group">
+    <div class="col-4 input-group">
         <div class="input-group-prepend">
             <span for="searchName" type="button" class="input-group-text"><i class="fas fa-search"></i></span>
         </div>
         <input class="form-control" v-model="searchName" aria-describedby="searchName">
     </div>
 
-    <div class="col-2 input-group">
+    <div class="col-4 input-group">
         <div class="input-group-prepend">
             <span for="hp" type="button" class="input-group-text"><i class="fas fa-heart"></i></span>
         </div>
         <input type="number" class="form-control" v-model.number="hp" aria-describedby="hp">
     </div>
 
-    <div class="col-2 input-group">
+    <div class="col-4 input-group">
         <div class="input-group-prepend">
             <span for="limit" type="button" class="input-group-text">LIMIT</span>
         </div>
         <input type="number" class="form-control" v-model.number="limit" aria-describedby="limit">
-    </div>
-
-    <div class="col-2">
-        <button v-on:click="order=!order" type="button" class="btn btn-light btn-outline-primary font-weight-bold">ID SORT</button>
     </div>
 </div>
 </template>
